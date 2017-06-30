631599c067bb9f4775796eea0815f3597f0963d5
pitsolu <pitsolu@gmail.com>
2017-07-01 01:43:59 +0300

    enable app gen with appname, updated composer.json

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,5 @@
 error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 
 $loader = require 'vendor/autoload.php';
-$loader->add('Payroll', __DIR__.'/app/src');
+//$loader->add('APP_FOLDER', __DIR__.'/app/src');
 $loader->add('App', __DIR__.'/lib/');
