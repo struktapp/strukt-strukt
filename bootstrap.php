0c99e8dba9960244aa160bcbff648863961b15a6
pitsolu <pitsolu@gmail.com>
2017-07-03 02:21:07 +0300

    load app via app.ini cfg file

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,8 @@
 
 error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 
+$appCfg = parse_ini_file("cfg/app.ini");
+
 $loader = require 'vendor/autoload.php';
-//$loader->add('APP_FOLDER', __DIR__.'/app/src');
+$loader->add($appCfg["app-name"], __DIR__.'/app/src');
 $loader->add('App', __DIR__.'/lib/');
