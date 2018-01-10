fbc533789241e5bcf48eb8518cede38afd8b11f8
pitsolu <pitsolu@gmail.com>
2018-01-10 14:06:31 +0300

    boiler plate in bootstrap, composer use php 7.0, clean up

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
+use Kambo\Http\Message\Environment\Environment;
+use Kambo\Http\Message\Factories\Environment\ServerRequestFactory;
+use Kambo\Http\Message\Response;
+
+use Strukt\Fs;
+use Strukt\Event\Single;
+
 error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 
 $appCfg = parse_ini_file("cfg/app.ini");
@@ -7,3 +14,44 @@ $appCfg = parse_ini_file("cfg/app.ini");
 $loader = require 'vendor/autoload.php';
 $loader->add($appCfg["app-name"], __DIR__.'/app/src');
 $loader->add('App', __DIR__.'/lib/');
+
+//
+
+// error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_WARNING);
+
+// $appCfg = parse_ini_file("cfg/app.ini");
+
+// $loader = require 'vendor/autoload.php';
+// $loader->add('App', __DIR__.'/lib/');
+// $loader->add('Strukt', __DIR__.'/../strukt-commons/src/');
+// $loader->add('Strukt', __DIR__.'/../strukt-router/src/');
+// $loader->add($appCfg["app-name"], __DIR__.'/app/src/');
+
+$registry = \Strukt\Core\Registry::getInstance();
+$registry->set("_dir", __DIR__);
+$registry->set("_staticDir", __DIR__."/public/static");
+
+$registry->set("servReq", new Single(function(){
+
+	if(empty($_SERVER["REQUEST_SCHEME"]))
+			$_SERVER["REQUEST_SCHEME"] = "http";
+
+	$env = new Environment($_SERVER, fopen('php://input', 'w+'), $_POST, $_COOKIE, $_FILES);
+
+	$servReq = (new ServerRequestFactory())->create($env);
+
+	return $servReq;
+}));
+
+foreach(["NotFound"=>404, "MethodNotFound"=>405,
+		 	"Forbidden"=>403, "ServerError"=>500,
+			"Ok"=>200, "Redirected"=>302] as $msg=>$code)
+	$registry->set(sprintf("Response.%s", $msg), new Single(function() use($code){
+
+		$res = new Response($code);
+
+		if(in_array($code, array(403,404,405,500)))
+			$res->getBody()->write(Fs::cat(sprintf("public/errors/%d.html", $code)));
+
+		return $res;
+	}));
--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 require "bootstrap.php";
 
-// Strukt\Rest\Request::shortenUrl(false);
-Strukt\Rest\Router::useStaticDir(__DIR__."/public/static");
+// Strukt\Router::useStaticDir(__DIR__."/public/static");
 
-$r = \Strukt\Framework\Registry::getInstance();
-$r->set("_dir", __DIR__);
+// $r = \Strukt\Framework\Registry::getInstance();
+// $r->set("_dir", __DIR__);
 
-App\Loader::getApp()->run();
-// App\Loader::getApp()->runDebug();
+// App\Loader::getApp()->run();
+App\Loader::getApp()->runDebug();
